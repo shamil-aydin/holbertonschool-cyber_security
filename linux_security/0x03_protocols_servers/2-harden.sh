f0d5789766c41f62c66e9fd7da5d3b62f37f4d12
shamil-aydin <13754@holbertonstudents.com>
2026-05-10 14:37:57 +0000

    Fix task 2 - use -0002 permission

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -xdev -type d -perm -o+w 2>/dev/null | tee /dev/stderr | xargs chmod o-w 2>/dev/null
+find / -xdev -type d -perm -0002 2>/dev/null | tee /dev/stderr | xargs chmod o-w 2>/dev/null
