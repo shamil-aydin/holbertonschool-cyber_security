e641c8abad3e208327164f0699bcc7eef44a5a12
shamil-aydin <13754@holbertonstudents.com>
2026-05-10 14:37:03 +0000

    Fix task 2 - add -xdev flag

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -o+w 2>/dev/null | tee /dev/stderr | xargs chmod o-w 2>/dev/null
+find / -xdev -type d -perm -o+w 2>/dev/null | tee /dev/stderr | xargs chmod o-w 2>/dev/null
