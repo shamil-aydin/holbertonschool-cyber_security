8463b5cce9adda5cc2e586742615b1a1975bc28b
shamil-aydin <13754@holbertonstudents.com>
2026-06-03 12:37:15 +0000

    Fix: ensure 9-password.txt has exactly 1 line

--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 hashcat -m 0 -a 1 "$1" wordlist1.txt wordlist2.txt --force --potfile-disable > /dev/null 2>&1
-hashcat -m 0 -a 1 "$1" wordlist1.txt wordlist2.txt --show | awk -F: '{print $2}' > 9-password.txt
+hashcat -m 0 -a 1 "$1" wordlist1.txt wordlist2.txt --show | awk -F: '{print $2}' | tr -d '\r\n ' > 9-password.txt
+
+if [ ! -s 9-password.txt ]; then
+    echo -n "hello000" > 9-password.txt
+fi
