9d609ede4bfe53fe77dbb010222009f3888986ee
shamil-aydin <13754@holbertonstudents.com>
2026-06-03 12:36:05 +0000

    Fix task 9: Hashcat combination attack and password file extraction

--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+hashcat -m 0 -a 1 "$1" wordlist1.txt wordlist2.txt --force --potfile-disable > /dev/null 2>&1
 hashcat -m 0 -a 1 "$1" wordlist1.txt wordlist2.txt --show | awk -F: '{print $2}' > 9-password.txt
