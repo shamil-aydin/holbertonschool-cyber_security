f039b15d5b1950c8e22828d26e22460b65b625cd
shamil-aydin <13754@holbertonstudents.com>
2026-06-09 17:00:27 +0000

    Fix task 5 format

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-domain="$1"
-
-subfinder -silent -d "$domain" | while read sub; do
-    ip=$(dig +short "$sub" | head -n 1)
-    echo "$sub,$ip"
-done > "${domain}.txt"
+subfinder -silent -d "$1" | xargs -I {} sh -c 'dig +short {} | head -n1 | awk -v s="{}" "{print s\",\"$0}"' > "$1.txt"
