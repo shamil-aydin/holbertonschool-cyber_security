34c1670d050afd4b1ef16f82ecc874b175c5a56b
shamil-aydin <14754@holbertonstudents.com>
2026-06-16 18:08:10 +0000

    Fix: Optimize script to 2 lines for checker requirement

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Argumanın verilib-verilmədiyini yoxlayır
-if [ -z "$1" ]; then
-    echo "İstifadə qaydası: $0 <subnetwork>"
-    exit 1
-fi
-
-# nmap vasitəsilə ARP skanını işə salır:
-# -sn: Port skanını deaktiv edir (ping scan)
-# -PR: ARP skanını məcbur edir
-sudo nmap -sn -PR "$1"
+sudo nmap -PR -sn "$1"
