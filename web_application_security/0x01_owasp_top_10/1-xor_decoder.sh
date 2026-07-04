e16b68183f2053a2adbcde17225f403fe160f964
shamil-aydin <13754@holbertonstudents.com>
2026-07-04 22:08:48 +0400

    Update 1-xor_decoder.sh

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,31 +1,7 @@
-#!/bin/bash
-
 if [ -z "$1" ]; then
+    echo "Usage: $0 {xor}HASH"
     exit 1
 fi
 
-# {xor} prefiksini silirik
-input_hash="${1#"{xor}"}"
-
-# Base64-dən açırıq (xxd əvəzinə 'od -An -v -tx1' istifadə edərək təmiz hex alırıq)
-hex_data=$(echo -n "$input_hash" | base64 -d 2>/dev/null | od -An -v -tx1 | tr -d ' \n')
-
-if [ -z "$hex_data" ]; then
-    exit 1
-fi
-
-decoded_output=""
-
-# Hər 2 hex simvolunu götürüb 95 (_) ilə XOR edirik
-for (( i=0; i<${#hex_data}; i+=2 )); do
-    byte_hex="${hex_data:$i:2}"
-    byte_dec=$((16#$byte_hex))
-    
-    # WebSphere daxili XOR açarı: 95
-    xor_dec=$(( byte_dec ^ 95 ))
-    
-    decoded_char=$(printf "\\$(printf '%03o' $xor_dec)")
-    decoded_output="${decoded_output}${decoded_char}"
-done
-
-echo -n "$decoded_output"
+encoded_string=$(echo "$1" | sed 's/^{xor}//')
+echo -n "$encoded_string" | base64 -d | perl -pe '$_ ^= "_" x length'
