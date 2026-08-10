7eff52a43cf920a2c2642926c16cc35b70590a7a
shamil-aydin <13754@holbertonstudents.com>
2026-08-10 10:05:02 +0400

    Modify attack_ip.sh to accept log file as argument

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $1}' logs.txt | grep -E '^[0-9]' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+awk '{print $1}' ${1:-logs.txt} | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'
